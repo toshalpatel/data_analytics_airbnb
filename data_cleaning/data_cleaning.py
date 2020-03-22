f5e62f0627851a4ef18324c7d47d147fdf09a3d4
Toshal Patel <toshaltpatel@gmail.com>
2020-03-23 01:25:42 +0800

    Update data_cleaning.py

--- data_cleaning/data_cleaning.py
+++ data_cleaning/data_cleaning.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[151]:
+# In[1]:
 
 
 import numpy as np
@@ -12,26 +12,26 @@ import math
 from collections import Counter
 
 
-# In[312]:
+# In[46]:
 
 
 imp_cols = "id,last_scraped,host_id,host_since,host_location,host_response_time,host_response_rate,host_acceptance_rate,host_is_superhost,host_neighbourhood,instant_bookable,review_scores_checkin,review_scores_value,reviews_per_month,review_scores_communication,review_scores_location,calculated_host_listings_count,calculated_host_listings_count_shared_rooms,calculated_host_listings_count_private_rooms,calculated_host_listings_count_entire_homes,require_guest_profile_picture,require_guest_phone_verification,cancellation_policy,is_business_travel_ready,host_listings_count,host_total_listings_count,host_identity_verified,neighbourhood,neighbourhood_cleansed,neighbourhood_group_cleansed,city,zipcode,market,smart_location,latitude,longitude,property_type,room_type,accommodates,bathrooms,bedrooms,beds,bed_type,amenities,price,weekly_price,monthly_price,security_deposit,cleaning_fee,guests_included,extra_people,minimum_nights,maximum_nights,minimum_minimum_nights,maximum_minimum_nights,minimum_maximum_nights,maximum_maximum_nights,minimum_nights_avg_ntm,calendar_updated,has_availability,availability_30,availability_60,availability_90,availability_365,calendar_last_scraped,number_of_reviews,number_of_reviews_ltm,first_review,last_review,review_scores_rating,review_scores_accuracy,review_scores_cleanliness"
 imp_cols_list = imp_cols.split(",")
 print(imp_cols_list)
 
-data = pd.read_csv("/Users/toshalpatel/is5152_project/concatenated_listings.csv", usecols=imp_cols_list)
+data = pd.read_csv("/Users/toshalpatel/is5152_project/data_cleaning/concatenated_listings.csv", usecols=imp_cols_list)
 
 
 # ## Exploring the data stats
 
-# In[313]:
+# In[4]:
 
 
 print("Number of rows: ",len(data.index))
 print("Number of cols: ",len(data.columns))
 
 
-# In[10]:
+# In[5]:
 
 
 data.head()
@@ -39,7 +39,7 @@ data.head()
 
 # #### Number of null values in each of the columns
 
-# In[13]:
+# In[6]:
 
 
 data.isna().sum()
@@ -49,7 +49,7 @@ data.isna().sum()
 
 # ### Dropping null values and retaining imp rows and cols
 
-# In[317]:
+# In[47]:
 
 
 data.drop(['host_acceptance_rate','weekly_price','monthly_price'], axis=1, inplace=True)
@@ -62,17 +62,18 @@ print("Number of cols: ",len(data.columns))
 
 # ### Defining new DataFrame
 
-# In[318]:
+# In[8]:
 
 
 df = pd.DataFrame()
 df['listing_id'] = data['id']
 df['host_id'] = data['host_id']
+df['last_scraped'] = data['last_scraped']
 
 
 # ## Defining some cleaning functions
 
-# In[319]:
+# In[9]:
 
 
 #calculate day difference    
@@ -102,7 +103,7 @@ def get_int(x,s):
 # 
 # We calculate the experience of the hosts based on how long he has been on the AirBnb platform.
 
-# In[320]:
+# In[10]:
 
 
 data['last_scraped_datetime']=data['last_scraped'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d"))
@@ -122,14 +123,14 @@ print(df['experience'].head())
 
 # Unique values in host response time
 
-# In[321]:
+# In[11]:
 
 
 print(data['host_response_time'].unique())
 df['host_response_time'] = data['host_response_time']
 
 
-# In[322]:
+# In[12]:
 
 
 print(data['host_response_rate'].unique())
@@ -138,7 +139,7 @@ df['host_response_rate'] = data['host_response_rate'].apply(lambda x : get_int(x
 print(df['host_response_rate'].head())
 
 
-# In[323]:
+# In[13]:
 
 
 print(data['host_is_superhost'].unique())
@@ -146,14 +147,14 @@ df['host_is_superhost'] = data['host_is_superhost'].apply(lambda x: get_bool(x))
 print(df['host_is_superhost'].head(),"\n",data['host_is_superhost'].head())
 
 
-# In[324]:
+# In[14]:
 
 
 print(data['host_total_listings_count'].head())
 df['host_total_listings_count'] = data['host_total_listings_count']
 
 
-# In[325]:
+# In[15]:
 
 
 print(data['host_identity_verified'].head())
@@ -161,7 +162,7 @@ df['host_identity_verified'] = data['host_identity_verified'].apply(lambda x: ge
 print(df['host_identity_verified'].head())
 
 
-# In[326]:
+# In[16]:
 
 
 df['calculated_host_listings_count'] = data['calculated_host_listings_count']
@@ -186,7 +187,7 @@ df['calculated_host_listings_count_shared_rooms'] = data['calculated_host_listin
 
 # #### Neighbourhood
 
-# In[327]:
+# In[17]:
 
 
 print(data['neighbourhood'].unique())
@@ -199,7 +200,7 @@ df['borough'] = data['neighbourhood_group_cleansed']
 
 # #### Property
 
-# In[328]:
+# In[18]:
 
 
 print("property_type: ",data['property_type'].unique())
@@ -219,21 +220,21 @@ df['bed_type'] = data['bed_type']
 
 # #### Amentities provided in the bnb
 
-# In[329]:
+# In[48]:
 
 
 print(data['amenities'].unique())
 
 
-# In[330]:
+# In[57]:
 
 
 print(len(data.index))
 data['amenities_n'] = data['amenities'].apply(lambda x : str(x).replace('{','').replace('}','')
-                        .replace('"','').lower().replace('nan','').split(',')).head()
+                        .replace('"','').lower().replace('nan','').split(','))
 
 
-# In[331]:
+# In[59]:
 
 
 amenities = []
@@ -252,19 +253,18 @@ def convert_to_none(x):
         
 data['amenities_n'] = data['amenities_n'].apply(lambda x: convert_to_none(x))
 data['amenities_n'].apply(lambda x: get_list_data(x))
-print(amenities)
 
 
-# In[332]:
+# In[61]:
 
 
 #print(dict((x,amenities.count(x)) for x in set(amenities)))
-print(dict(Counter(amenities).most_common(5)))
+print(dict(Counter(amenities).most_common(6)))
 
 
-# Hence, {'wifi': 5, 'kitchen': 5, 'heating': 5, 'smoke detector': 5, 'carbon monoxide detector': 5} are the TOP 5 amentities 
+# Hence, {'wifi': 242492, 'heating': 237638, 'essentials': 236237, 'smoke detector': 223918, 'kitchen': 223320} are the TOP 5 amentities 
 
-# In[333]:
+# In[63]:
 
 
 def if_amenities(x, a):
@@ -283,23 +283,22 @@ def get_count(x):
         return None
     
 df['amenities_count'] = data['amenities_n'].apply(lambda x: get_count(x))
-print(df['amenities_count'].head())
 df['is_wifi'] = data['amenities_n'].apply(lambda x : if_amenities(x,'wifi'))
 df['is_kitchen'] = data['amenities_n'].apply(lambda x : if_amenities(x,'kitchen'))
 df['is_heating'] = data['amenities_n'].apply(lambda x : if_amenities(x,'heating'))
 df['is_smoke_detector'] = data['amenities_n'].apply(lambda x : if_amenities(x,'smoke detector'))
-df['is_carbon_mo_detector'] = data['amenities_n'].apply(lambda x : if_amenities(x,'carbon monoxide detector'))
+df['is_aircon'] = data['amenities_n'].apply(lambda x : if_amenities(x,'air conditioning'))
 
 
 # ### Price
 
-# In[334]:
+# In[24]:
 
 
 print(data['price'].unique())
 
 
-# In[337]:
+# In[25]:
 
 
 df['price'] = data['price'].apply(lambda x: float(str(x).replace(',','').strip('$')))
@@ -310,7 +309,7 @@ df['price_for_extra_people'] = data['extra_people'].apply(lambda x: float(str(x)
 
 # ### Stay
 
-# In[338]:
+# In[26]:
 
 
 df['guests_included'] = data['guests_included']
@@ -325,7 +324,7 @@ df['is_instant_bookable'] = data['instant_bookable'].apply(lambda x: get_bool(x)
 df['is_business_travel_ready'] = data['is_business_travel_ready'].apply(lambda x: get_bool(x))
 
 
-# In[339]:
+# In[27]:
 
 
 print(data['cancellation_policy'].unique())
@@ -334,7 +333,7 @@ df['cancellation_policy'] = data['cancellation_policy']
 
 # #### steps to be taken for the guests
 
-# In[340]:
+# In[28]:
 
 
 df['require_guest_profile_picture'] = data['require_guest_profile_picture'].apply(lambda x: get_bool(x))
@@ -343,7 +342,7 @@ df['require_guest_phone_verification'] = data['require_guest_phone_verification'
 
 # ### Reviews of the listing
 
-# In[341]:
+# In[29]:
 
 
 df['number_of_reviews'] = data['number_of_reviews']
@@ -358,32 +357,9 @@ df['review_scores_location'] = data['review_scores_location']
 df['review_scores_value'] = data['review_scores_value']
 
 
-# In[342]:
+# In[70]:
 
 
-columns = df.columns
-
-
-# In[343]:
-
-
-columns
-
-
-# In[344]:
-
-
-len(columns)
-
-
-# In[345]:
-
-
-df.to_csv("Jan2019-Feb2020_cleaned_data.csv", index=None)
-
-
-# In[ ]:
-
-
+df.to_csv("/Users/toshalpatel/is5152_project/Jan2019-Feb2020_cleaned_data.csv", index=None)
 
 
